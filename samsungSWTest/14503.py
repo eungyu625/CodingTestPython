3f5e2f318e3892c714f8e27fb5a9986676f72a7a
LeeEunGyu <agyu625@gmail.com>
2022-04-29 22:31:06 +0900

    BOJ 문제풀이

---
+++ samsungSWTest/14503.py
@@ -0,0 +1,39 @@
+
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+n, m = map(int, input().split())
+r, c, d = map(int, input().split())
+
+arr = []
+check = [[0] * m for _ in range(n)]
+
+for _ in range(n):
+    arr.append(list(map(int, input().split())))
+
+check[r][c] = 1
+ans = 1
+
+while True:
+    found = False
+    for _ in range(4):
+        nd = (d + 3) % 4
+        nx = r + dx[nd]
+        ny = c + dy[nd]
+        d = nd
+        if 0 <= nx < n and 0 <= ny < m:
+            if arr[nx][ny] == 0 and check[nx][ny] == 0:
+                check[nx][ny] = 1
+                ans += 1
+                r = nx
+                c = ny
+                found = True
+                break
+
+    if not found:
+        if arr[r - dx[d]][c - dy[d]] == 1:
+            print(ans)
+            break
+        else:
+            r = r - dx[d]
+            c = c - dy[d]
