c7a2c05b3d42c7e878793929a35ddd49d73a4ac2
LeeEunGyu <agyu625@gmail.com>
2022-05-26 17:33:22 +0900

    BOJ 문제풀이

--- samsungSWTest/15683.py
+++ samsungSWTest/15683.py
@@ -1,54 +1,58 @@
 import copy
+import sys
 
-mode = [[], [[0], [1], [2], [3]], [[0, 2], [1, 3]], [[0, 1], [1, 2], [2, 3], [3, 0]],
-        [[0, 1, 2], [0, 1, 3], [0, 2, 3], [1, 2, 3]], [[0, 1, 2, 3]]]
+road = [[], [[0], [1], [2], [3]], [[0, 1], [2, 3]], [[2, 1], [1, 3], [3, 0], [0, 2]],
+        [[0, 1, 2], [1, 2, 3], [0, 1, 3], [0, 2, 3]], [[0, 1, 2, 3]]]
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+dx = [0, 0, -1, 1] # E W N S
+dy = [1, -1, 0, 0]
+
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = []
-q = []
+cctv = []
+cnt = 0
 ans = int(1e9)
-cctv = 0
 
 for i in range(n):
     data = list(map(int, input().split()))
     arr.append(data)
     for j in range(len(data)):
         if data[j] != 0 and data[j] != 6:
-            cctv += 1
-            q.append([i, j, data[j]])
+            cctv.append([i, j])
+            cnt += 1
 
 
-def oversee(y, x, direction, temp):
-    for d in direction:
-        nx = x
-        ny = y
+def oversee(temp, number, direction):
+    for d in road[number][direction]:
+        x, y = dx[d], dy[d]
         while True:
-            nx += dx[d]
-            ny += dy[d]
-            if 0 <= nx < m and 0 <= ny < n and arr[ny][nx] != 6:
-                if arr[ny][nx] == 0:
-                    temp[ny][nx] = '#'
-            else:
+            if 0 > x or n <= x or 0 > y or m <= y or temp[x][y] == 6:
                 break
+            if temp[x][y] == 0:
+                temp[x][y] = '#'
+            x += dx[d]
+            y += dy[d]
 
 
-def solve(current, cnt):
-    global ans
-    if cnt == cctv:
+def solve(temp, count):
+    if count == cnt:
+        global ans
         res = 0
-        for a in current:
-            res += a.count(0)
+        for a in range(n):
+            for b in range(m):
+                if temp[a][b] == 0:
+                    res += 1
         ans = min(ans, res)
         return
-    temp = copy.deepcopy(current)
-    y, x, direction = q[cnt]
-    for a in mode[direction]:
-        oversee(y, x, a, temp)
-        solve(temp, cnt + 1)
-        temp = copy.deepcopy(current)
+
+    current = copy.deepcopy(temp)
+    cctv_num = arr[cctv[count][0]][cctv[count][1]]
+    for d in road[cctv_num]:
+        oversee(current, cctv_num, d)
+        solve(current, count + 1)
+        current = copy.deepcopy(temp)
 
 
 solve(arr, 0)
--- samsungSWTest/15686.py
+++ samsungSWTest/15686.py
@@ -1,68 +1,35 @@
 
-from collections import deque
+import sys
+from itertools import combinations
 
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
+input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = []
-
-for _ in range(n):
-    arr.append(list(map(int, input().split())))
-
-chicken = 0
-house = 0
-queue = deque()
+home = []
+store = []
 
 for i in range(n):
-    for j in range(n):
-        if arr[i][j] == 2:
-            chicken += 1
-            queue.append((i, j))
-        if arr[i][j] == 1:
-            house += 1
+    data = list(map(int, input().split()))
+    for j in range(len(data)):
+        if data[j] == 2:
+            store.append([i, j])
+        elif data[j] == 1:
+            home.append([i, j])
 
-ans = [[] * chicken for _ in range(chicken)]
-check = [0] * chicken
+ans = int(1e9)
 
-res = 0
-while queue:
-    x, y = queue.popleft()
-    for a in range(n):
-        for b in range(n):
-            if arr[a][b] == 1:
-                dist = abs(x - a) + abs(y - b)
-                ans[res].append(dist)
-    res += 1
+survive = list(combinations(store, m))
 
-result = 2500
+for i in range(len(survive)):
+    res = 0
+    for [x, y] in home:
+        dist = 101
+        for [a, b] in survive[i]:
+            dist = min(dist, abs(x - a) + abs(y - b))
+        res += dist
+    ans = min(res, ans)
 
-
-def distance(index, city, mid):
-    if index == house:
-        global result
-        result = min(result, mid)
-        return
-
-    min_num = 50
-    for c in range(len(city)):
-        min_num = min(min_num, city[c][index])
-    distance(index + 1, city, mid + min_num)
+print(ans)
 
 
-def solve(index, start, city):
-    if index == m:
-        distance(0, city, 0)
-        return
-
-    for c in range(start, len(ans)):
-        if check[c] == 0:
-            check[c] = 1
-            city.append(ans[c])
-            solve(index + 1, c + 1, city)
-            city.pop()
-            check[c] = 0
-
-
-solve(0, 0, [])
-print(result)
