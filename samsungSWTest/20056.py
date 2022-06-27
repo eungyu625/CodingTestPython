2c2fa09bf9d7fb440026e2667e045b18e23d6408
LeeEunGyu <agyu625@gmail.com>
2022-06-27 21:52:36 +0900

    BOJ 문제풀이

--- samsungSWTest/20056.py
+++ samsungSWTest/20056.py
@@ -1,173 +1,40 @@
-import copy
+N, M, K = map(int, input().split())
+fireballs = []
+for _ in range(M):
+    fireballs.append(list(map(int, input().split())))
 
 dx = [-1, -1, 0, 1, 1, 1, 0, -1]
 dy = [0, 1, 1, 1, 0, -1, -1, -1]
 
-# 0 1 2 3 4 5 6 7
+map = [[[] for _ in range(N + 1)] for _ in range(N + 1)]
 
-n, m, k = map(int, input().split())
-fireball = []
-arr = [[0] * n for _ in range(n)]
-ans = 0
+for _ in range(K):
+    while fireballs:
+        r, c, m, s, d = fireballs.pop(0)
+        nr = (r + s * dx[d]) % N
+        nc = (c + s * dy[d]) % N
+        map[nr][nc].append([m, s, d])
 
-for _ in range(m):
-    ri, ci, mi, si, di = map(int, input().split())
-    fireball.append([ri, ci, mi, si, di])
-    arr[ri - 1][ci - 1] = mi
-
-for ii in range(n):
-    for jj in range(n):
-        print(arr[ii][jj], end=' ')
-    print()
-print()
-
-
-def divide():
-    global fireball
-    res = []
-    while fireball:
-        current = [fireball[0]]
-        x = fireball[0][0]
-        y = fireball[0][1]
-        for fire in range(1, len(fireball)):
-            if x == fireball[fire][0] and y == fireball[fire][1]:
-                current.append(fireball[fire])
-        if len(current) == 1:
-            continue
-        for cur in current:
-            fireball.remove(cur)
-        mass = 0
-        velocity = 0
-        val = current[0][4] % 2
-        direction = True
-        for cur in current:
-            mass += cur[2]
-            velocity += cur[3]
-            if cur[4] % 2 != val:
-                direction = False
-        mass //= 5
-        velocity //= len(current)
-        if mass == 0:
-            continue
-
-        if direction:
-            res.append([x, y, mass, velocity, 0])
-            res.append([x, y, mass, velocity, 2])
-            res.append([x, y, mass, velocity, 4])
-            res.append([x, y, mass, velocity, 6])
-        else:
-            res.append([x, y, mass, velocity, 1])
-            res.append([x, y, mass, velocity, 3])
-            res.append([x, y, mass, velocity, 5])
-            res.append([x, y, mass, velocity, 7])
-
-    for value in res:
-        fireball.append(value)
-
-
-def move():
-
-    for ball in fireball:
-        if ball[4] == 0:
-            if ball[0] + ball[3] * dx[ball[4]] < 1:
-                ball[0] = n + 1 + ball[3] * dx[ball[4]]
-            else:
-                ball[0] += ball[3] * dx[ball[4]]
-        elif ball[4] == 1:
-            if ball[0] + ball[3] * dx[ball[4]] < 1 and ball[1] + ball[3] * dy[ball[4]] > n:
-                ball[0] = n + 1 + ball[3] * dx[ball[4]]
-                ball[1] = ball[1] + ball[3] * dy[ball[4]] - n
-            else:
-                if ball[0] + ball[3] * dx[ball[4]] < 1:
-                    ball[0] = n + 1 + ball[3] * dx[ball[4]]
-                    ball[1] += ball[3] * dy[ball[4]]
-                elif ball[1] + ball[3] * dy[ball[4]] > n:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] = ball[1] + ball[3] * dy[ball[4]] - n
+    for i in range(1, N + 1):
+        for j in range(1, N + 1):
+            if len(map[i][j]) > 1:
+                sum_m, sum_s, cnt_odd, cnt_even, cnt = 0, 0, 0, 0, len(map[i][j])
+                while map[i][j]:
+                    _m, _s, _d = map[i][j].pop(0)
+                    sum_m += _m
+                    sum_s += _s
+                    if _d % 2:
+                        cnt_odd += 1
+                    else:
+                        cnt_even += 1
+                if cnt_odd == cnt or cnt_even == cnt:
+                    nd = [0, 2, 4, 6]
                 else:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] += ball[3] * dy[ball[4]]
-        elif ball[4] == 2:
-            if ball[1] + ball[3] * dy[ball[4]] > n:
-                ball[1] = ball[1] + ball[3] * dy[ball[4]] - n
-            else:
-                ball[1] += ball[3] * dy[ball[4]]
-        elif ball[4] == 3:
-            if ball[0] + ball[3] * dx[ball[4]] > n and ball[1] + ball[3] * dy[ball[4]] > n:
-                ball[0] = ball[0] + ball[3] * dx[ball[4]] - n
-                ball[1] = ball[1] + ball[3] * dy[ball[4]] - n
-            else:
-                if ball[0] + ball[3] * dx[ball[4]] > n:
-                    ball[0] = ball[0] + ball[3] * dx[ball[4]] - n
-                    ball[1] += ball[3] * dy[ball[4]]
-                elif ball[1] + ball[3] * dy[ball[4]] > n:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] = ball[1] + ball[3] * dy[ball[4]] - n
-                else:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] += ball[3] * dy[ball[4]]
-        elif ball[4] == 4:
-            if ball[0] + ball[3] * dx[ball[4]] > n:
-                ball[0] = ball[0] + ball[3] * dx[ball[4]] - n
-            else:
-                ball[0] += ball[3] * dx[ball[4]]
-        elif ball[4] == 5:
-            if ball[0] + ball[3] * dx[ball[4]] > n and ball[1] + ball[3] * dy[ball[4]] < 1:
-                ball[0] = ball[0] + ball[3] * dx[ball[4]] - n
-                ball[1] = n + 1 + ball[3] * dy[ball[4]]
-            else:
-                if ball[0] + ball[3] * dx[ball[4]] > n:
-                    ball[0] = ball[0] + ball[3] * dx[ball[4]] - n
-                    ball[1] += ball[3] * dy[ball[4]]
-                elif ball[1] + ball[3] * dy[ball[4]] < 1:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] = n + 1 + ball[3] * dy[ball[4]]
-                else:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] += ball[3] * dy[ball[4]]
-        elif ball[4] == 6:
-            if ball[1] + ball[3] * dy[ball[4]] < 1:
-                ball[1] = n + 1 + ball[3] * dy[ball[4]]
-            else:
-                ball[1] += ball[3] * dy[ball[4]]
-        elif ball[4] == 7:
-            if ball[0] + ball[3] * dx[ball[4]] < 1 and ball[0] + ball[3] * dy[ball[4]] < 1:
-                ball[0] = n + 1 + ball[3] * dx[ball[4]]
-                ball[1] = n + 1 + ball[3] * dy[ball[4]]
-            else:
-                if ball[0] + ball[3] * dx[ball[4]] < 1:
-                    ball[0] = n + 1 + ball[3] * dx[ball[4]]
-                    ball[1] += ball[3] * dy[ball[4]]
-                elif ball[1] + ball[3] * dy[ball[4]] < 1:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] = n + 1 + ball[3] * dy[ball[4]]
-                else:
-                    ball[0] += ball[3] * dx[ball[4]]
-                    ball[1] += ball[3] * dy[ball[4]]
-    current = [[0] * n for _ in range(n)]
-    for p in range(n):
-        for q in range(n):
-            for ball in fireball:
-                if p == ball[0] - 1 and q == ball[1] - 1:
-                    current[p][q] += ball[2]
+                    nd = [1, 3, 5, 7]
+                if sum_m // 5:
+                    for k in nd:
+                        fireballs.append([i, j, sum_m // 5, sum_s // cnt, k])
+            if len(map[i][j]) == 1:
+                fireballs.append([r, c] + map[r][c].pop())
 
-
-for i in range(k):
-    move()
-    divide()
-    temp = [[0] * n for _ in range(n)]
-    for a in range(n):
-        for b in range(n):
-            for fb in fireball:
-                if a == fb[0] - 1 and b == fb[1] - 1:
-                    temp[a][b] += fb[2]
-    for a in range(n):
-        for b in range(n):
-            print(temp[a][b], end=' ')
-        print()
-    print()
-
-for fb in fireball:
-    ans += fb[2]
-
-print(ans)
+print(sum([f[2] for f in fireballs]))
