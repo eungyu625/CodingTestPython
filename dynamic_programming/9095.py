0daec7a97899b8beb394d7d0e35a2696062e9a57
Eungyu-code <Agyu625@gmail.com>
2022-08-19 16:46:46 +0900

    BOJ 문제풀이

--- dynamic_programming/9095.py
+++ dynamic_programming/9095.py
@@ -1,16 +1,9 @@
-
-t = int(input())
-
-
-while t:
+for tc in range(int(input())):
     n = int(input())
-    dp = [0] * 11
-    dp[1] = 1  # 1
-    dp[2] = 2  # 1+1, 2
-    dp[3] = 4  # 1+1+1, 1+2, 2+1, 3
-
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 4
     for i in range(4, n + 1):
-        dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1]
-
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3]
     print(dp[n])
-    t -= 1
